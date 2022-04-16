d5da9ad828a9302a998f4d895381acbdef11d4a2
Isaiah Cedano <isaiahcedano6@gmail.com>
2022-04-16 11:55:53 -0500

    apr 16 update

--- main.js
+++ main.js
@@ -256,12 +256,226 @@ const resetPdscourses = async () => {
   }
 }
 
-app.get("/pdscourses", async (req, res) => {
-  res.json(data);
+app.get("/products", async (req, res) => {
+  const products = Object.entries(data).reduce((curr, [category, courses]) => {
+    return {
+      ...curr,
+      [category]: courses.reduce((curr, {title, salesPage, description, imgSrc}) => {
+        return [
+          ...curr,
+          {
+            title,
+            imgSrc,
+            salesPage,
+            description,
+            price: '2.99',
+          }
+        ]
+      }, [])
+    }
+  }, {})
+  res.json(products);
 });
 
+// app.get("/pdscourses", async (req, res) => {
+//   /*
+//   {
+//     category: [
+//       {
+//         title: "",
+//         downloadLink: {
+//           mega: "",
+//           koofr: ""
+//         },
+//         password: {
+//           mega: "",
+//           koofr: "",
+//         },
+//         salesPage: "",
+//         description: "",
+//       }
+//     ]
+//   }
+//   */
+//   try {
+//     // Login into pdscourses
+//     const {url, cookie} = await pdscoursesLogin;
+//
+//     // PDSCourses Home Page
+//     // const homePage = await jQueryWebsite("https://www.pdscourses.com/", cookie);
+//     // End
+//
+//     // PDSCourses categorical pages
+//     let categoricalPages = [{
+//       title: "Self Improvement",
+//       link: "https://www.pdscourses.com/hypnosis-nlp-psychology/",
+//       rangeCategory: 11
+//     }];
+//
+//     /*
+//       ,
+//       ,
+//       ,
+//     */
+//
+//     // homePage("a.fusion-button")
+//     //   .filter((index, item) => index>=7&&!homePage(item).text().includes("All"))
+//     //   .map((index, ele) => (
+//     //     {
+//     //       title: homePage(ele).text(),
+//     //       link: homePage(ele).attr("href"),
+//     //     }
+//     //   ))
+//     //   .each((index, item) => {
+//     //     categoricalPages.push(item);
+//     //   });
+//     // End
+//
+//     const productPages = await Promise.all(
+//       // promises that will resolve into a list of categories with
+//       // their corresponding products.
+//       categoricalPages.map(async ({title, link, rangeCategory}) => {
+//         // For each category product page, it'll loop through
+//         // the range to get all the available products.
+//         let pages = [];
+//         const range = [...Array(rangeCategory).keys()];
+//         const pagePromises = range.map(async num => {
+//           const pagePromise = new Promise(async (resolve, reject) => {
+//             try {
+//               const $ = await jQueryWebsite(`${link}page/${num+1}`, cookie);
+//               resolve($);
+//             } catch(err) {
+//               reject("error");
+//             }
+//           });
+//           return await pagePromise;
+//         });
+//         // It'll return a list of promises that'll resolve into
+//         // the category product page number.
+//         const arr = await Promise.all(pagePromises);
+//         // arr = category product page number. A list of product pages according
+//         // to the category.
+//
+//         // For this category, loop through each product page number, select all the titles,
+//         // and add them into the pages array.
+//         await Promise.all(arr.map(async page => {
+//           return await Promise.all(page("h2.entry-title > a").map(async (index, item) => {
+//             const pageLink = page(item).attr("href");
+//             let salesPage = "";
+//             let description = "";
+//             let imgSrc = "";
+//             const megaLink = [];
+//             const koofrLink = [];
+//             const mediaLink = [];
+//             const megaPasswords = [];
+//             const mediaPasswords = [];
+//             const koofrPasswords = [];
+//             const passwords = [];
+//             try {
+//               const productPage = await jQueryWebsite(pageLink, cookie);
+//
+//               // Set product salespage
+//               productPage("a").each((index, item) => {
+//                 if (productPage(item).text().includes("Sales")) {
+//                   salesPage = productPage(item).attr("href");
+//                 }
+//               });
+//               // End
+//
+//               // Set product description
+//               if (productPage("h3").length) {
+//                 productPage("h3")
+//                 .each((index, item) => {
+//                   if (
+//                     !productPage(item).text().includes("SIZE:") &&
+//                     !productPage(item).text().includes("Size:") &&
+//                     productPage(item).text() !== "DOWNLOAD" &&
+//                     productPage(item).text() !== "Download"
+//                   ) {
+//                     description = `${description ? description + " " : ''}${productPage(item).text()}`
+//                   }
+//                 });
+//               }
+//               // End
+//
+//               // Set product link
+//               if (productPage("a").length) {
+//                 productPage("a").each((index, ele) => {
+//                   if (productPage(ele).attr("href")
+//                       .includes("mega.nz")) {
+//                     megaLink.push(productPage(ele).attr("href"));
+//                   } else if (productPage(ele).attr("href").includes("k00.fr")) {
+//                     koofrLink.push(productPage(ele).attr("href"));
+//                   } else if (productPage(ele).attr("href").includes("mediafire.com")) {
+//                     mediaLink.push(productPage(ele).attr("href"));
+//                   }
+//                 });
+//               }
+//               // End
+//
+//               // Set product password
+//               if (productPage("p, h2, h3, h4, h5, h6, span").length) {
+//                 productPage("p, h2, h3, h4, h5, h6, span").each((index, ele) => {
+//                   if (productPage(ele).text().includes("Password")) {
+//                     passwords.push(productPage(ele).text());
+//                   }
+//                 })
+//               }
+//
+//               // Set image source
+//               if (productPage("img").length) {
+//                 productPage("img").each((index, ele) => {
+//                   if (index === 1) {
+//                     imgSrc = productPage(ele).attr("src");
+//                   }
+//                 })
+//
+//               }
+//
+//               pages.push({
+//                 salesPage,
+//                 description,
+//                 imgSrc,
+//
+//                 title: page(item).text(),
+//                 downloadLink: {
+//                   mega: megaLink,
+//                   koofr: koofrLink,
+//                   media: mediaLink,
+//                 },
+//                 password: passwords,
+//               });
+//               return true;
+//             } catch(err) {
+//               console.log(err);
+//               return false;
+//             }
+//           }));
+//         }));
+//
+//         // return a category as key and pages as value.
+//         return {
+//           [title]: pages,
+//         }
+//       })
+//     );
+//
+//     res.send(productPages.reduce((curr, item) => {
+//       const course = Object.entries(item)[0];
+//       const category = course[0];
+//       const products = course[1];
+//       return {
+//         ...curr,
+//         [category]: products,
+//       }
+//     }, {}));
+//   } catch(err) {
+//     console.log(err);
+//     res.send("failed");
+//   }
+// })
 
-const lPort = process.env.PORT || 3000;
+const lPort = process.env.PORT || 3001;
 
 app.listen(lPort, () => {
   console.log(`listening to port ${lPort}`);
